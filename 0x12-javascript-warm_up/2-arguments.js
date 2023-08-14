32598d1ea279f72ad4d697e4086960a3fe4dc8c8
AworThales <thals4jesus@gmail.com>
2023-08-14 06:00:38 -0700

    fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
-
-const { argv } = require('process');
-const my_argc = argv.length;
-
-if (my_argc === 2) {
+if (process.argv.length === 2) {
   console.log('No argument');
-} else if (my_argc === 3) {
+} else if (process.argv.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
