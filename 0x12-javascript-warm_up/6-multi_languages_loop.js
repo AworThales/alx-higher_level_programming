aef083b286b55972c4a534dade64e296f0af532b
AworThales <thals4jesus@gmail.com>
2023-08-14 06:17:56 -0700

    fix

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,3 @@
 #!/usr/bin/node
-// ['C is fun', 'Python is cool', 'JavaScript is amazing'].forEach((e) => console.log(e));
 const my_msg = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 my_msg.forEach((element) => console.log(element));
